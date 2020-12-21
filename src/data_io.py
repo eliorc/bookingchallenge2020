e53b33d56a1706d5570535b3ae9f52702003acf7
elior <elior.cohen.p@gmail.com>
2020-12-21 19:05:21 +0200

    :sparkles: Data loading function to accept root dir

--- src/data_io.py
+++ src/data_io.py
@@ -1,14 +1,16 @@
 import warnings
-
-import conf
+from pathlib import Path
 
 import pandas as pd
 
+import conf
 
-def load_raw_data() -> pd.DataFrame:
+
+def load_raw_data(data_dir: Path = conf.DATA_DIR) -> pd.DataFrame:
     """
     Load training set as given by booking
 
+    :param data_dir: Root data dir
     :return: Raw train set
     """
 
@@ -16,14 +18,14 @@ def load_raw_data() -> pd.DataFrame:
     with warnings.catch_warnings():
         warnings.simplefilter(action='ignore', category=FutureWarning)
 
-        return pd.read_csv(conf.DATA_DIR / 'raw/booking_train_set.csv', dtype={'user_id': 'int32',
-                                                                               # check(in|out) be parsed in parse_dates
-                                                                               'checkin': 'str',
-                                                                               'checkout': 'str',
-                                                                               'city_id': 'int32',
-                                                                               'affiliate_id': 'int32',
-                                                                               'booker_country': 'str',
-                                                                               'hotel_country': 'str',
-                                                                               'utrip_id': 'str'},
+        return pd.read_csv(data_dir / 'raw/booking_train_set.csv', dtype={'user_id': 'int32',
+                                                                          # check(in|out) be parsed in parse_dates
+                                                                          'checkin': 'str',
+                                                                          'checkout': 'str',
+                                                                          'city_id': 'int32',
+                                                                          'affiliate_id': 'int32',
+                                                                          'booker_country': 'str',
+                                                                          'hotel_country': 'str',
+                                                                          'utrip_id': 'str'},
                            parse_dates=['checkin', 'checkout'],
                            index_col=0)
