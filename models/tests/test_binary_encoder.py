39dcd20b3b064dcebf9de44b5597b4bd43dbbf26
elior <elior.cohen.p@gmail.com>
2020-12-29 13:54:55 +0200

    :children_crossing: BinaryEncoder to support not only numbers

--- models/tests/test_binary_encoder.py
+++ models/tests/test_binary_encoder.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 import numpy as np
 import pytest
 
@@ -13,12 +15,42 @@ from models.transformers import BinaryEncoder
                                                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]]))],
 )
-def test_encoder_output(inputs: np.ndarray, outputs: np.ndarray):
+def test_encoder_output_no_encoding(inputs: np.ndarray, outputs: np.ndarray):
     """
     Check that binary encoder encodes as expected different types of input
     """
 
     binary_encoder = BinaryEncoder()
+
+    transformed = binary_encoder.fit_transform(inputs)
+
+    assert np.all(transformed == outputs)
+
+
+@pytest.mark.parametrize(
+    "inputs,outputs,encode",
+    [(np.array([[1, 'no'],
+                [0, 'yes'],
+                [5, 'yes']]),
+      np.array([[0, 0, 1, 0],
+                [0, 0, 0, 1],
+                [1, 0, 1, 1]]),
+      [False, True]),
+     (np.array([[25, 'no'],
+                [30, 'yes'],
+                [55, 'yes']]),
+      np.array([[0, 0, 0],
+                [0, 1, 1],
+                [1, 0, 1]]),
+      [True, True])])
+def test_encoder_output_with_encoding(inputs: np.ndarray,
+                                      outputs: np.ndarray,
+                                      encode: Optional[List[bool]]):
+    """
+    Check that binary encoder encodes as expected different types of input, using internal encoding
+    """
+
+    binary_encoder = BinaryEncoder(pre_encode=encode)
 
     transformed = binary_encoder.fit_transform(inputs)
 
