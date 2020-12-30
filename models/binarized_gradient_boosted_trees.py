76b763e50bedcbd16b81770210cf75467d41ba33
elior <elior.cohen.p@gmail.com>
2020-12-30 10:20:41 +0200

    :building_construction: Delegate encoding responsibility out of BinaryEncoder

--- models/binarized_gradient_boosted_trees.py
+++ models/binarized_gradient_boosted_trees.py
@@ -5,54 +5,67 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 
 from models.trans_prob import fit_pipeline as fit_trans_prob_pipeline
-from models.transformers import BinaryEncoder
+from models.transformers import BinaryEncoder, CyclicEncoder
 
 
 class ModelingTable(TransformerMixin, BaseEstimator):
 
+    def __init__(self, prediction_table):
+        self.prediction_table = prediction_table
+
     # noinspection PyAttributeOutsideInit
     def fit(self, X, y=None):
-        # Prepare the affiliate encoder, support unknown affiliates (-1)
-        self.affiliate_encoder = LabelEncoder().fit([-1] + X['affiliate_id'].tolist())
-        assert self.affiliate_encoder.classes_[0] == -1
+        # Prepare the affiliate and hotel country encoder, support unknown affiliates (-1)
+        self.affiliate_encoder_ = LabelEncoder().fit([-1] + X['affiliate_id'].tolist())
+        assert self.affiliate_encoder_.classes_[0] == -1
 
-        # Use the benchmark model to get the prediction table
-        # where prediction_table[i,j] is the jth most probable city to travel to from i (0 is most probable)
-        self.prediction_table = fit_trans_prob_pipeline(X, y).steps[-1][1].prediction_table_
+        self.hotel_country_encoder_ = LabelEncoder().fit([-1] + X['hotel_country'].tolist())
+        assert self.hotel_country_encoder_.classes_[0] == '-1'
+
+        self.booker_country_encoder_ = LabelEncoder().fit(X['booker_country'].tolist())
 
         return self
 
     # noinspection PyPep8Naming
-    def transform(self, X):
-        # Encode affiliate_ids
-        known_affiliates = set(self.affiliate_encoder.classes_)
-        X.loc[:, 'affiliate_id'] = X['affiliate_id'].apply(
+    def transform(self, features: pd.DataFrame):
+        # Encode affiliate_ids and hotel_country
+        known_affiliates = set(self.affiliate_encoder_.classes_)
+        known_hotel_countries = set(self.hotel_country_encoder_.classes_)
+
+        data = features.copy()
+
+        # Replace unknowns and encode
+        data.loc[:, 'affiliate_id'] = data['affiliate_id'].apply(
             lambda affiliate_id: affiliate_id if affiliate_id in known_affiliates else -1)
-        X.loc[:, 'affiliate_id'] = self.affiliate_encoder.transform(X['affiliate_id'])
+        data.loc[:, 'affiliate_id'] = self.affiliate_encoder_.transform(data['affiliate_id'])
+        data.loc[:, 'hotel_country'] = data['hotel_country'].apply(
+            lambda hotel_country: hotel_country if hotel_country in known_hotel_countries else '-1')
+        data.loc[:, 'hotel_country'] = self.hotel_country_encoder_.transform(data['hotel_country'])
+        data.loc[:, 'booker_country'] = self.booker_country_encoder_.transform(data['booker_country'])
 
         # Reorder for relevant features
-        X = X.groupby('utrip_id', sort=False).agg({
+        data = data.groupby('utrip_id', sort=False).agg({
             'city_id': ['first', 'last'],
             'booker_country': 'last',
             'affiliate_id': 'last',
             'hotel_country': ['first', 'last'],
             'checkin': 'first',
             'checkout': 'last'}).reset_index()
-        X.columns = ['_'.join(col).rstrip('_') for col in X.columns.values]  # Flatten column names
+        data.columns = ['_'.join(col).rstrip('_') for col in data.columns.values]  # Flatten column names
 
         # Create features - week numbers
-        X['first_week_number'] = X['checkin_first'].dt.isocalendar().week
-        X['last_week_number'] = X['checkout_last'].dt.isocalendar().week
+        data['first_week_number'] = data['checkin_first'].dt.isocalendar().week - 1  # -1 to go from 0 to 52, this is
+        data['last_week_number'] = data['checkout_last'].dt.isocalendar().week - 1  # relevant for cyclic encoder
 
         # Prepare top 3 destinations for binarization
-        X['most_probable_city_0'] = X['city_id_last'].apply(lambda x: self.prediction_table[x, 0])
-        X['most_probable_city_1'] = X['city_id_last'].apply(lambda x: self.prediction_table[x, 1])
-        X['most_probable_city_2'] = X['city_id_last'].apply(lambda x: self.prediction_table[x, 2])
+        data['most_probable_city_0'] = data['city_id_last'].apply(lambda x: self.prediction_table[x, 0])
+        data['most_probable_city_1'] = data['city_id_last'].apply(lambda x: self.prediction_table[x, 1])
+        data['most_probable_city_2'] = data['city_id_last'].apply(lambda x: self.prediction_table[x, 2])
 
         # Drop irrelevant features
-        X = X.drop(columns=['checkin_first', 'checkout_last', 'utrip_id'])
+        data = data.drop(columns=['checkin_first', 'checkout_last', 'utrip_id'])
 
-        return X
+        return data
 
 
 # noinspection PyUnusedLocal
@@ -67,20 +80,22 @@ def fit_pipeline(features: pd.DataFrame, labels: pd.DataFrame, n_cities: int, **
     :return: Fitted pipeline
     """
 
+    # Use the benchmark model to get the prediction table
+    # where prediction_table[i,j] is the jth most probable city to travel to from i (0 is most probable)
+    prediction_table = fit_trans_prob_pipeline(features, labels).steps[-1][1].prediction_table_
+
     # Prerequisites
     n_affiliates = features.affiliate_id.nunique() + 1  # +1 for unknown
+    n_hotel_countries = features.hotel_country.nunique() + 1
     n_booker_countries = features.booker_country.nunique()
-    n_hotel_countries = features.hotel_country.nunique()
 
     # Build pipeline
-    pipeline = Pipeline([('modeling_table', ModelingTable()),
+    pipeline = Pipeline([('modeling_table', ModelingTable(prediction_table=prediction_table)),
                          ('binarize_features', ColumnTransformer([('binarizer',
                                                                    BinaryEncoder(
                                                                        [n_cities, n_cities, n_cities, n_cities,
                                                                         n_cities, n_booker_countries, n_affiliates,
-                                                                        n_hotel_countries, n_hotel_countries],
-                                                                       [False, False, False, False, False, True, True,
-                                                                        True, True]),
+                                                                        n_hotel_countries, n_hotel_countries]),
                                                                    ['city_id_first',
                                                                     'city_id_last',
                                                                     'most_probable_city_0',
@@ -89,9 +104,12 @@ def fit_pipeline(features: pd.DataFrame, labels: pd.DataFrame, n_cities: int, **
                                                                     'booker_country_last',
                                                                     'affiliate_id_last',
                                                                     'hotel_country_first',
-                                                                    'hotel_country_last'],
-                                                                   )], remainder='passthrough'))])
+                                                                    'hotel_country_last']),
+                                                                  ('cyclic_encoder',
+                                                                   CyclicEncoder(maximum=[52, 52]),
+                                                                   ['first_week_number', 'last_week_number'])],
+                                                                 remainder='passthrough'))])
     # Fit
-    pipeline.fit(features, labels['city_id'].values)
+    pipeline.fit(features, labels)
 
     return pipeline
--- models/transformers/binary_encoder.py
+++ models/transformers/binary_encoder.py
@@ -2,7 +2,6 @@ from typing import Optional, List
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils import check_array
 
@@ -31,18 +30,13 @@ class BinaryEncoder(TransformerMixin, BaseEstimator):
          [1 0 0]]
     """
 
-    def __init__(self, maximum: Optional[List[int]] = None,
-                 pre_encode: Optional[List[bool]] = None):
+    def __init__(self, maximum: Optional[List[int]] = None):
         """
 
         :param maximum: Maximum values per column
-        :param pre_encode: Pre encode variables before binary encoding. This needs to be done for features which are
-            not integers going from 0 onwards and for features that are not integers at all. Defaults to true for all
-            features
         """
 
         self.maximum = maximum
-        self.pre_encode = pre_encode
 
     # noinspection PyUnusedLocal,PyAttributeOutsideInit
     def fit(self, X, y=None):
@@ -51,29 +45,14 @@ class BinaryEncoder(TransformerMixin, BaseEstimator):
         check_array(X, dtype=None)
         data = X.values if isinstance(X, pd.DataFrame) else X
 
-        # Prepare pre encoders
-        if not self.pre_encode:
-            self.pre_encode = [False] * data.shape[1]
-        self.pre_encoders = [LabelEncoder() if encode else None for encode in self.pre_encode]
-
-        # Train encoders
-        for i, encoder in enumerate(self.pre_encoders):
-            if encoder:
-                encoder.fit(data[:, i])
-
         # Figure out the number of columns needed to represent all the numbers
         if not self.maximum:
-            self.maximum = list()
-            for i, encoder in enumerate(self.pre_encoders):
-                if encoder:  # If the column is to be encoded
-                    self.maximum.append(len(encoder.classes_) - 1)
-                else:  # If not, it should be a numerical column from 0 onwards
-                    self.maximum.append(np.max(data[:, i].astype(int)))
+            self.maximum = np.max(data, axis=0).astype(int)
 
         self.depth_ = list(map(self._get_depth, self.maximum))
 
         # Get relevant indices for trimming all zero columns
-        self._relevant_indices = [slice(-d, None) for d in self.depth_]
+        self.relevant_indices_ = [slice(-d, None) for d in self.depth_]
 
         return self
 
@@ -81,13 +60,7 @@ class BinaryEncoder(TransformerMixin, BaseEstimator):
 
         # Input validation
         check_array(X, dtype=None)
-
-        data = X.values if isinstance(X, pd.DataFrame) else X
-
-        # Encode
-        for i, encoder in enumerate(self.pre_encoders):
-            if encoder:
-                data[:, i] = encoder.transform(data[:, i])
+        data = X.values.copy() if isinstance(X, pd.DataFrame) else X.copy()
 
         data = data.astype(int)
 
@@ -112,7 +85,7 @@ class BinaryEncoder(TransformerMixin, BaseEstimator):
             return array
 
         # Convert to bits
-        transformed = np.concatenate([to_bits(data[:, i], indices) for i, indices in enumerate(self._relevant_indices)],
+        transformed = np.concatenate([to_bits(data[:, i], indices) for i, indices in enumerate(self.relevant_indices_)],
                                      axis=-1)
 
         return transformed
