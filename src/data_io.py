2f3ae03af89243dea2a00ca58253ae2bea62814f
elior <elior.cohen.p@gmail.com>
2021-01-12 17:38:04 +0200

    :sparkles: Loading function pre-split train/validation/test

--- src/data_io.py
+++ src/data_io.py
@@ -1,7 +1,9 @@
+import random
 import warnings
 from pathlib import Path
 from typing import Tuple
 
+import numpy as np
 import pandas as pd
 
 import conf
@@ -92,3 +94,50 @@ def separate_features_from_label(raw_data: pd.DataFrame) -> Tuple[pd.DataFrame, 
     labels = raw_data[label_mask][['utrip_id', 'city_id']]
 
     return features, labels
+
+
+def load_split_train_validation_test(data_dir: Path = conf.DATA_DIR,
+                                     validation_frac: float = 0.1,
+                                     test_frac: float = 0.1,
+                                     trip_length_threshold: int = 0,
+                                     seed: int = 1729) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Split train, validation and test from the raw data. Split is made by utrip IDs
+
+    :param data_dir: Root data dir
+    :param validation_frac: Fraction of entire data to assign to validation
+    :param test_frac: Fraction of entire data to assign to test
+    :param trip_length_threshold: If supplied, will filter out trips that have less destinations than the threshold
+    :param seed: Random seed to use, for reproducibility
+    :return: Split data
+    """
+
+    # Prerequisites
+    random.seed(seed)
+    np.random.seed(seed)
+
+    # Get raw data
+    raw_data = load_raw_data(data_dir=data_dir, trip_length_threshold=trip_length_threshold)
+
+    # Extract trip IDs, to split by
+    all_trips = set(raw_data.utrip_id)
+
+    # Figure out sizes for each split
+    test_size = int(len(all_trips) * test_frac)
+    validation_size = int(len(all_trips) * validation_frac)
+
+    # Assign trips for each split
+    test_trips = np.random.choice(list(all_trips), size=test_size, replace=False)
+    all_trips -= set(test_trips)
+    validation_trips = np.random.choice(list(all_trips), size=validation_size, replace=False)
+    train_trips = all_trips - set(validation_trips)
+
+    # Validate split is valid
+    assert not train_trips.intersection(validation_trips, test_trips)
+
+    # Validate we have all the trips assigned
+    assert set(raw_data.utrip_id) == train_trips.union(test_trips).union(validation_trips)
+
+    return raw_data[raw_data.utrip_id.isin(train_trips)], \
+           raw_data[raw_data.utrip_id.isin(validation_trips)], \
+           raw_data[raw_data.utrip_id.isin(test_trips)]
